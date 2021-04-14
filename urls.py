3443fecd904bc131a9806b34c1b5239a13e54220
Christian de Lima A. Zangirolamo <34872215+Leizen@users.noreply.github.com>
2021-04-14 16:27:42 -0300

    Delete urls.py

--- urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path('', views.login, name='login'),
-    path('cadastro', views.cadastro, name='cadastro'),
-    path('logado', views.login_success, name='logado')
-]
